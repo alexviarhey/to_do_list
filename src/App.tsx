546d59b4cf81410661fbb6519842a38ba6fecc10
alexviarhey <lexa.vergey@gmail.com>
2019-10-29 20:20:12 +0300

    style update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
+import {ITodolist} from "./types/common-types";
+import TodoList from './components/TodoList';
+import AddNewItemForm from './components/AddNewItemForm';
+import { connect } from 'react-redux';
+import {addTodolist} from "./redux/todolist-reducer";
+import {AppStateType} from "./redux/redux-store";
+import Header from './components/Header';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+interface IMapStateProps {
+    todolists: Array<ITodolist>
 }
 
-export default App;
+interface IMapDispatchProps {
+    addTodo: (newTodolist: ITodolist) => void
+}
+
+type AppProps = IMapStateProps & IMapDispatchProps
+
+
+
+const App = (props: AppProps) => {
+
+    const [nextTodoListId, setNextTodolistId] = useState(0);
+
+
+    const addTodoList = (title: string): void => {
+        let newTodoList: ITodolist = {id: nextTodoListId, title, tasks: []};
+        setNextTodolistId(nextTodoListId + 1);
+        props.addTodo(newTodoList)
+    };
+
+    const todolists = props.todolists.map(tl => <TodoList key={tl.id} title={tl.title} id={tl.id} tasks={tl.tasks} />);
+
+    return (
+        <div className='App'>
+            <Header />
+            <div className='listWrapper'>
+                <div className='materialForm'>
+                    <AddNewItemForm addItem={addTodoList} />
+                </div>
+                <div className='todolistsWrapper'>
+                    {todolists}
+                </div>
+            </div>
+        </div>
+    )
+};
+
+const mapStateToProps = (state: AppStateType): IMapStateProps => {
+    return {
+        todolists: state.todolists.todolists
+    }
+};
+
+const mapDispatchToProps = (dispatch: any): IMapDispatchProps => {
+    return {
+        addTodo: (newTodolist) => dispatch(addTodolist(newTodolist))
+    }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
